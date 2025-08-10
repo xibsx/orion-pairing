aede86b0821d5d7bf4d9b01d5e90235964f92fcb
xibsx <xibstech@gmail.com>
2025-08-10 11:38:05 +0300

    Create package.js

---
+++ package.js
@@ -0,0 +1,19 @@
+{
+  "name": "orion-pairing",
+  "version": "1.0.0",
+  "description": "WhatsApp Bot Pairing Website",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js",
+    "dev": "nodemon server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "cors": "^2.8.5",
+    "dotenv": "^16.0.3",
+    "axios": "^1.6.2"
+  },
+  "devDependencies": {
+    "nodemon": "^3.0.2"
+  }
+}
